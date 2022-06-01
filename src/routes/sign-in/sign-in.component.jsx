5046f5621ad00d37924ada3fa2dad5d297940ff9
Srutip04 <subuhishruti75@gmail.com>
2022-06-01 19:13:14 +0530

    setting up user documents

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,12 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () =>{
     const logGoogleUser = async () => {
-        const respone = await signInWithGooglePopup();
-        console.log(respone);
+        const {user}= await signInWithGooglePopup();
+        createUserDocumentFromAuth(user);
     }
     return(
         <div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,7 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
-
+import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore';
+import { async } from "@firebase/util";
 const firebaseConfig = {
   apiKey: "AIzaSyBOU88AgCO7i5SYauDnPMAkfKPnaRJhKMM",
   authDomain: "fashlux-db.firebaseapp.com",
@@ -25,3 +26,13 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup= () => signInWithPopup(auth,provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) =>{
+  const userDocRef = doc(db, 'users' , userAuth.uid );  
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+}
