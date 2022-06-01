5d32d657efa2517cb5ed5d9c8a1e86626e9d6d95
Srutip04 <subuhishruti75@gmail.com>
2022-06-01 17:43:32 +0530

    sign-in component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () =>{
   return(
@@ -13,9 +13,10 @@ const App = () =>{
 
  return (
    <Routes>
-     <Route path='/' element={<Navigation />}>
-        <Route  index element={<Home />} />
-        <Route path="shop"  element={<Shop />} />
+     <Route path="/" element={<Navigation />}>
+       <Route index element={<Home />} />
+       <Route path="shop" element={<Shop />} />
+       <Route path="sign-in" element={<SignIn />} />
      </Route>
    </Routes>
  );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,22 +3,24 @@ import { Link, Outlet } from "react-router-dom";
 import {ReactComponent as Logo}  from '../../assets/crown (1).svg'
 import './naviagtion.styles.scss'
 const Navigation = () =>{
-  return(
-
-  <Fragment>
+  return (
+    <Fragment>
       <div className="navigation">
-          <Link className="logo-container" to='/'>
-            <Logo className="logo"></Logo>
+        <Link className="logo-container" to="/">
+          <Logo className="logo"></Logo>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
           </Link>
-          <div className="nav-links-container">
-              <Link className="nav-link" to='/shop'>
-                  SHOP
-              </Link>
-          </div>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
       </div>
       <Outlet />
-  </Fragment>
-  )
+    </Fragment>
+  );
 }
 
 export default Navigation;
