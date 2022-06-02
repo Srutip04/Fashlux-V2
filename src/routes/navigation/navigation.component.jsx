c39e55b27e907c9ec7d6f4ee895e8a800619f4e5
Srutip04 <subuhishruti75@gmail.com>
2022-06-02 16:31:47 +0530

    cart-icon & dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment , useContext} from "react";
 import { Link, Outlet } from "react-router-dom";
-import {ReactComponent as Logo}  from '../../assets/crown (1).svg'
+import {ReactComponent as Logo}  from '../../assets/crown (1).svg';
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import './naviagtion.styles.scss'
@@ -28,7 +30,9 @@ const Navigation = () =>{
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
