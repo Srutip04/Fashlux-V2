e4dff7a4674653c7bdcc84e5dd800f45c36f71c6
Srutip04 <subuhishruti75@gmail.com>
2022-06-03 20:48:53 +0530

    switching productsContext to categoriesContext and using categoryMap

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
-import { ProductsProvider } from "./contexts/products.contexts";
+import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
-import "./index.scss";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,28 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.contexts";
+import { useContext, Fragment } from "react";
+
 import ProductCard from "../../components/product-card/product-card.component";
-import './shop.styles.scss'
-const Shop = () =>{
-    const {products} = useContext(ProductsContext)
-    return(
-        <div className="products-container">
-            {products.map((product) =>(
-                <ProductCard key={product.id} product={product} />
+
+import { CategoriesContext } from "../../contexts/categories.context";
+
+import "./shop.styles.scss";
+
+const Shop = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
             ))}
-            
-        </div>
-    )
-} 
+          </div>
+        </Fragment>
+      ))}
+    </Fragment>
+  );
+};
 
 export default Shop;
