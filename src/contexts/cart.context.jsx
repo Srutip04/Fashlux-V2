5446caee4e4293c0bce58eef38e62158aa7ed3d7
Srutip04 <subuhishruti75@gmail.com>
2022-06-03 12:53:27 +0530

    cart item count functionality added

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,18 +1,18 @@
-import { createContext, useState ,useEffect} from "react";
+import { createContext, useState, useEffect } from "react";
 
-const addCartItem = (cartItems, productToAdd) => {
+export const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
-    (cartItem) => (cartItem = productToAdd.id)
+    (cartItem) => cartItem.id === productToAdd.id
   );
 
   if (existingCartItem) {
-    return cartItems.map(
-      (cartItem) =>
-        (cartItem.id = productToAdd.id
-          ? { ...cartItem, quantity: cartItem.quantity + 1 }
-          : cartItem)
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
   }
+
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
@@ -21,22 +21,32 @@ export const CartContext = createContext({
   setIsOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
-  cartCount: 0
+  cartItemCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [cartCount,setCartCount] = useState(0);
+  const [cartItemCount, setCartItemCount] = useState(0);
 
-  useEffect(() =>{
-   const newCartCount = cartItems.reduce((total,cartItem) =>total + cartItem.quantity,0)
-   setCartCount(newCartCount)
-  },[cartItems])
+  useEffect(() => {
+    const count = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartItemCount(count);
+  }, [cartItems]);
 
-  const addItemToCart = (productToAdd) => {
-    setCartItems(addCartItem(cartItems, productToAdd));
+  const addItemToCart = (product) =>
+    setCartItems(addCartItem(cartItems, product));
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartItemCount,
   };
-  const value = { isCartOpen, setIsCartOpen ,addItemToCart,cartItems,cartCount};
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
