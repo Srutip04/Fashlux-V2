4d9311ca1f9bc910bb94f002b930776198e6ca0a
Srutip04 <subuhishruti75@gmail.com>
2022-06-10 00:31:08 +0530

    userReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,17 +1,46 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect,useReducer } from "react";
 import {
   onAuthStateChangedListener,
   signOutUser,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
 //as rhe actual value you want to access
+
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer =  (state,action) =>{
+  const {type,payload} = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+
+}
+
+const INITIAL_STATE ={
+  currentUser: null,
+}
+
 export const UserProvider = ({children}) =>{
-    const [currentUser, setCurrentUser] = useState(null);
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [{currentUser},dispatch] = useReducer(userReducer,INITIAL_STATE);
+
+    const setCurrentUser = (user) =>{
+      dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    }
+  
     const value = {currentUser,setCurrentUser};
 
     // signOutUser();
