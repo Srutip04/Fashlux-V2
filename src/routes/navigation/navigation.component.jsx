6f038e61454326002f980dd196028f84e8fa3fcd
Srutip04 <subuhishruti75@gmail.com>
2022-06-11 17:30:49 +0530

    user reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,47 +1,50 @@
-import { Fragment , useContext} from "react";
-import { Link, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux/es/exports";
-import {ReactComponent as Logo}  from '../../assets/crown (1).svg';
+import { Fragment, useContext } from "react";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import { CartContext } from "../../contexts/cart.context";
 import { selectCurrentUser } from "../../store/user/user.selector";
-import { CartContext, CartProvider } from "../../contexts/cart.context";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown (1).svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-import {NavigationContainer,LogoContainer,NavLinks,NavLink} from './naviagtion.styles.jsx'
 
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from "./navigation.styles.jsx";
 
-const Navigation = () =>{
+const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
-  const {isCartOpen} = useContext(CartContext)
-  console.log(currentUser);
-
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
       <NavigationContainer>
         <LogoContainer to="/">
-          <Logo/>
+          <CrwnLogo className="logo" />
         </LogoContainer>
         <NavLinks>
-          <NavLink to="/shop">
-            SHOP
-          </NavLink>
+          <NavLink to="/shop">SHOP</NavLink>
+
           {currentUser ? (
-            <NavLink as='span' onClick={signOutUser}>
+            <NavLink as="span" onClick={signOutUser}>
               SIGN OUT
             </NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
-              SIGN IN
-            </Link>
+            <NavLink to="/auth">SIGN IN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
-       {isCartOpen && <CartDropdown />}
+        {isCartOpen && <CartDropdown />}
       </NavigationContainer>
       <Outlet />
     </Fragment>
   );
-}
+};
 
 export default Navigation;
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,21 +1,16 @@
-import { USER_ACTION_TYPES } from "./user.types";
+import USER_ACTION_TYPES from "./user.types";
 
-const INITIAL_STATE = {
+export const USER_INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
+export const userReducer = (state = USER_INITIAL_STATE, action = {}) => {
   const { type, payload } = action;
 
   switch (type) {
     case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: payload,
-      };
+      return { ...state, currentUser: payload };
     default:
-    //   throw new Error(`Unhandled type ${type} in userReducer`);
-    return state;
+      return state;
   }
 };
-
