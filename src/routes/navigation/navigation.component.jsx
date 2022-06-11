d187875287e21d33d57930ae3ec9b37365419253
Srutip04 <subuhishruti75@gmail.com>
2022-06-11 17:20:59 +0530

    removing userContext and moving user auth listener into appjs

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,17 @@
 import { Fragment , useContext} from "react";
 import { Link, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux/es/exports";
 import {ReactComponent as Logo}  from '../../assets/crown (1).svg';
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { UserContext } from "../../contexts/user.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
 import { CartContext, CartProvider } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import {NavigationContainer,LogoContainer,NavLinks,NavLink} from './naviagtion.styles.jsx'
+
+
 const Navigation = () =>{
-  const {currentUser} = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const {isCartOpen} = useContext(CartContext)
   console.log(currentUser);
 
