14cb366c25827ca7ef9ba5c4713667951dad3ca4
Srutip04 <subuhishruti75@gmail.com>
2022-06-13 23:22:37 +0530

    added redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,8 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import {persistStore,persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk';
+
 import { rootReducer } from "./root-reducer";
 
 
@@ -14,7 +16,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 const middleWares = [
-  process.env.NODE_ENV === "development" && logger,
+  process.env.NODE_ENV === "development" && logger,thunk
 ].filter(Boolean);
 
 const composeEnhancer  = (process.env.NODE_ENV != 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
