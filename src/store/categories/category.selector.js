c2ea4add1ff04a0c07fcf69cbd6fa53f43faaf1d
Srutip04 <subuhishruti75@gmail.com>
2022-06-13 17:44:48 +0530

    reselect lib

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,18 @@
-export const selectCategoriesMap = (state) =>{
- console.log('selector fired')
- return state.categories.categories
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriresSlice) => categoriresSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>categories
  .reduce((acc,category) =>{
      const {title,items} = category;
      acc[title.toLowerCase()] = items;
      return acc;
-   },{});
-}
+   },{})
+) 
