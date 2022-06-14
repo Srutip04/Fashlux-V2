3918e55f804f900f6368b14cd0397a52d5397853
Srutip04 <subuhishruti75@gmail.com>
2022-06-14 20:56:13 +0530

    setting up payment form

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,31 @@
+import { async } from '@firebase/util';
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js'
+
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
+import { PaymentFormContainer, FormContainer } from "./payment-from.styles";
+
+const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentHandler = async (e) =>{
+        e.preventDefault();
+
+        if(!stripe || !elements){
+            return;
+        }
+
+        
+    }
+    return (
+      <PaymentFormContainer>
+        <FormContainer>
+            <h2>Credit Card Payment:</h2>
+          <CardElement />
+          <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+        </FormContainer>
+      </PaymentFormContainer>
+    );
+}
+
+export default PaymentForm;
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -6,7 +6,7 @@ import {
 } from "../../store/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-
+import PaymentForm from "../../components/payment-form/payment-form.component";
 import {
   CheckoutContainer,
   CheckoutHeader,
@@ -41,6 +41,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
