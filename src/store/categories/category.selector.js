da25ed3c933ed903716e63455cafa287e912fb99
Srutip04 <subuhishruti75@gmail.com>
2022-06-14 17:57:32 +0530

    used isLoading to render spinner

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -4,15 +4,20 @@ const selectCategoryReducer = (state) => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
-  (categoriresSlice) => categoriresSlice.categories
-)
+  (categoriesSlice) => categoriesSlice.categories
+);
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>categories
- .reduce((acc,category) =>{
-     const {title,items} = category;
-     acc[title.toLowerCase()] = items;
-     return acc;
-   },{})
-) 
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
+
+export const selectIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
